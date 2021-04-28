0de6968a082d70aa12d2b740fcb9ce775d2e4e95
Cyprien <cypri920@hotmail.fr>
2021-04-27 21:55:08 -0400

    Update Video 4

--- monster.py
+++ monster.py
@@ -1,5 +1,11 @@
 import pygame
 
 #créer une classe pour la notion de monstre
-class Monster:
+class Monster(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 5
+        self.image = pygame.image.load('assets/mummy.png')
+        self.rect = self.image.get_rect
